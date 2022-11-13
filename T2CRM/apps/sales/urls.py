b136d01f0fbf69a3e27a756181b562da857c689a
wangchen7152 <18335185814@163.com>
2022-11-13 16:58:09 +0800

    'update-sales'

--- T2CRM/apps/sales/urls.py
+++ T2CRM/apps/sales/urls.py
@@ -6,7 +6,8 @@ __author__ = 'wang'
 from .views import *
 from django.conf.urls import url
 from sales.views import SalesIndex, SalesList, CustomerCompany, \
-    CreateSaleChance, DelSaleChance
+    CreateSaleChance, DelSaleChance, SaleDevPlanIndex, SaleDevPlanDetail, \
+    DeleteSaleChance
 
 app_name = 'sales'
 urlpatterns = [
@@ -20,4 +21,19 @@ urlpatterns = [
         name='CreateSaleChance'),
     url(r'DelSaleChance/', csrf_exempt(DelSaleChance.as_view()),
         name='DelSaleChance'),
+    # 进入客户开发计划页面
+    url(r'SaleDevPlanIndex/', csrf_exempt(SaleDevPlanIndex.as_view()),
+        name='SaleDevPlanIndex'),
+    # 点击开发进入开发页面下详情
+    url(r'SaleDevPlanDetail/', csrf_exempt(SaleDevPlanDetail.as_view()),
+        name='SaleDevPlanDetail'),
+    # 查询计划项目
+    url(r'CusDevPlanList/', csrf_exempt(CusDevPlanList.as_view()),
+        name='CusDevPlanList'),
+    # 添加或编辑开发计划
+    url(r'AddOrUpdateSaleChance/', csrf_exempt(AddOrUpdateSaleChance.as_view()),
+        name='AddOrUpdateSaleChance'),
+    # 删除开发计划
+    url(r'DeleteSaleChance/', csrf_exempt(DeleteSaleChance.as_view()),
+        name='DeleteSaleChance'),
 ]
