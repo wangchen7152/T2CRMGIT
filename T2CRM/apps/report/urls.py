bf9d09305bdac0769e9e39a8eb5edc40d0053889
wangchen7152 <18335185814@163.com>
2022-11-21 21:22:21 +0800

    '1121'

--- T2CRM/apps/report/urls.py
+++ T2CRM/apps/report/urls.py
@@ -4,7 +4,9 @@ from django.views.decorators.csrf import csrf_exempt
 __author__ = 'wang'
 
 from django.conf.urls import url
-from report.views import ReportIndex,ReportCustomerSalePrice
+from report.views import ReportIndex, ReportCustomerSalePrice, \
+    CompositionCustomer, CustomerLossPage, CustomerContribute, SelectCustomerLevel, \
+    SelectCustomerServer
 
 app_name = 'report'
 urlpatterns = [
@@ -12,6 +14,23 @@ urlpatterns = [
     url(r'ReportIndex/$', csrf_exempt(ReportIndex.as_view()),
         name='ReportIndex'),
     # 进去客户销售页面
-    url(r'ReportCustomerSalePrice/$', csrf_exempt(ReportCustomerSalePrice.as_view()),
+    url(r'ReportCustomerSalePrice/$',
+        csrf_exempt(ReportCustomerSalePrice.as_view()),
         name='ReportCustomerSalePrice'),
+    # 进去客户构成分析页面
+    url(r'CompositionCustomer/$', csrf_exempt(CompositionCustomer.as_view()),
+        name='CompositionCustomer'),
+    # 进去客户流失页面
+    url(r'CustomerLossPage/$', csrf_exempt(CustomerLossPage.as_view()),
+        name='CustomerLossPage'),
+    # 进去客户贡献页面
+    url(r'CustomerContribute/$', csrf_exempt(CustomerContribute.as_view()),
+        name='CustomerContribute'),
+    # 获取客户构成分许图
+    url(r'SelectCustomerLevel/$', csrf_exempt(SelectCustomerLevel.as_view()),
+        name='SelectCustomerLevel'),
+    # 进去客户服务图
+    url(r'SelectCustomerServer/$', csrf_exempt(SelectCustomerServer.as_view()),
+        name='SelectCustomerServer'),
+
 ]
